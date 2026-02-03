37186c7ed2eb7e1af8289004e3d098e7b31c62a6
user.02 <hamidovzalina@gmail.com>
2026-02-03 12:38:25 +0300

    Stack

---
+++ Stack/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Stack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(29, 79);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(131, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(29, 126);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(131, 23);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(29, 180);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(131, 23);
+            this.buttonShow.TabIndex = 3;
+            this.buttonShow.Text = "Показать стек";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 247);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(331, 121);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 451);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+                string item = textBox1.Text;
+                if (!string.IsNullOrEmpty(item))
+                {
+                    stack.Push(item);
+                    textBox1.Clear();
+                    MessageBox.Show($"Элемент '{item}' добавлен в стек.");
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Введите элемент для добавления.");
+                }
+            
+        }
+
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (var item in stack)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Stack/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public partial class Form1 : Form
+    {
+
+        private Stack<string> stack = new Stack<string>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                stack.Push(item);
+                textBox1.Clear();
+                MessageBox.Show($"Элемент '{item}' добавлен в стек.");
+            }
+            else
+            {
+                MessageBox.Show("Введите элемент для добавления.");
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            if (stack.Count > 0)
+            {
+                string removedItem = stack.Pop();
+                MessageBox.Show($"элемент '{removedItem}' удален из стека.");
+            }
+            else
+            {
+                MessageBox.Show("Стек пуст.");
+            }
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var item in stack)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
