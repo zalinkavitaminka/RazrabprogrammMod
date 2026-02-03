ea544f6986c54dfb5f127f866dc3b029a9aaa350
user.02 <hamidovzalina@gmail.com>
2026-02-03 13:40:13 +0300

    Stack, Queue, отчет

---
+++ Queue/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Queue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                queue.Enqueue(item);
+                textBox1.Clear();
+                MessageBox.Show("$'Элемент '{item}' добавлен в очередь.");
+            }
+            else
+            {
+                MessageBox.Show("Введите элемент для добавления.");
+            }
+
+        }
+
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (var item in queue)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(23, 82);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(146, 30);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(23, 140);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(146, 30);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(23, 201);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(146, 30);
+            this.buttonShow.TabIndex = 3;
+            this.buttonShow.Text = "Показать очередь";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 271);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 121);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 471);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Queue/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    public partial class Form1 : Form
+    {
+
+        private Queue<string> queue = new Queue<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                queue.Enqueue(item);
+                textBox1.Clear();
+                MessageBox.Show("$'Элемент '{item}' добавлен в очередь.");
+            }
+            else
+            {
+                MessageBox.Show("Введите элемент для добавления.");
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (queue.Count > 0)
+            {
+                string removedItem = queue.Dequeue();
+                MessageBox.Show($"Элемент '{removedItem}' удален из очереди.");
+            }
+            else
+            {
+                MessageBox.Show("Очередь пуста.");
+            }
+
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var item in queue)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
+    }
+}
